6bbe9b32d9d31566a14b55b1e5a900dcd56ffd5c
seojin kim <001106ksj@gmail.com>
2022-03-01 17:47:02 +0900

    ✨ feat: 전화번호 입력창 만들기

--- presentation/screens/Signup/PhoneInput.tsx
+++ presentation/screens/Signup/PhoneInput.tsx
@@ -1,21 +1,162 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Text, Button } from 'react-native';
+import { Text, StyleSheet, View, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+import BottomSheet from 'react-native-gesture-bottom-sheet';
 import ButtonBack from '../../components/ButtonBack';
 import { NavigationStackParams } from '../../../infrastructures/types/NavigationStackParams';
+import ButtonNext from '../../components/ButtonNext';
+import COLORS from '../../styles/colors';
+import { Country } from '../../../infrastructures/types/country';
 
 type PhoneInputScreenProp = NativeStackScreenProps<
   NavigationStackParams,
   'PhoneInput'
 >;
 
+const countryCodeWithEmoji = require('../../../infrastructures/data/countryCodeWithEmoji.json');
+const countryCodeWithTelNumber: Country[] = require('../../../infrastructures/data/countryCodeWithTelNumber.json');
+const icToggleDown = require('../../../assets/ic-toggle-down.png');
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 16,
+    paddingTop: 12,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: 'Pretendard-Bold',
+    marginBottom: 12,
+  },
+  titleWrapper: {
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  countryInfoContainer: {
+    position: 'absolute',
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    height: 54,
+    left: 16,
+  },
+  countryCodeText: {
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 16,
+    marginStart: 5,
+  },
+  countryCodeSelectorContainer: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  countryFlagText: {
+    fontSize: 24,
+  },
+  textInput: {
+    paddingStart: 116,
+    paddingEnd: 16,
+    paddingVertical: 18,
+    borderRadius: 5,
+    backgroundColor: COLORS.GREY_030,
+    marginBottom: 12,
+    fontSize: 16,
+    borderColor: 'transparent',
+  },
+  countryCodeSelectorTitleText: {
+    marginTop: 66,
+    marginStart: 26,
+    marginBottom: 40,
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 20,
+  },
+  countryCodeSelectorWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingHorizontal: 26,
+    paddingVertical: 20,
+    alignItems: 'center',
+  },
+  countryCodeCountryNameText: {
+    fontFamily: 'Pretendard-Bold',
+    fontSize: 16,
+  },
+  countryCodeCountryCodeText: {
+    fontFamily: 'Pretendard',
+    marginStart: 5,
+  },
+});
+
 function PhoneInputScreen({ navigation }: PhoneInputScreenProp) {
+  const [selectedCountry, setSelectedCountry] = useState('KR');
+  const [phone, setPhone] = useState('');
+  const bottomSheetRef = useRef<any>(null);
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <BottomSheet ref={bottomSheetRef} snapPoints={[700]} height={700}>
+        <ScrollView>
+          <Text style={styles.countryCodeSelectorTitleText}>국가 코드</Text>
+          {countryCodeWithTelNumber.map((country) => (
+            <TouchableOpacity
+              onPress={() => {
+                setSelectedCountry(country.countryCode);
+                bottomSheetRef.current?.close();
+              }}
+              key={country.countryCode}
+              style={styles.countryCodeSelectorWrapper}
+            >
+              <Text style={styles.countryCodeCountryNameText}>
+                {country.countryNameKR}
+              </Text>
+              <Text style={styles.countryCodeCountryCodeText}>
+                +{country.countryPhoneNumber}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </BottomSheet>
       <ButtonBack onPress={() => navigation.goBack()} />
-      <Text>PhoneInput</Text>
-      <Button title="다음" onPress={() => navigation.navigate('PhoneAuth')} />
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>번호를 입력하세요</Text>
+      </View>
+      <View>
+        <TouchableOpacity
+          style={styles.countryInfoContainer}
+          onPress={() => bottomSheetRef.current?.show()}
+        >
+          <Text style={styles.countryFlagText}>
+            {countryCodeWithEmoji[selectedCountry]}
+          </Text>
+          <Text style={styles.countryCodeText}>
+            +
+            {countryCodeWithTelNumber.find(
+              (country: Country) => country.countryCode === selectedCountry,
+            )?.countryPhoneNumber ?? ''}
+          </Text>
+          <Image source={icToggleDown} style={{ width: 24, height: 24 }} />
+        </TouchableOpacity>
+        <TextInput
+          value={phone}
+          onChangeText={(text) => setPhone(text)}
+          style={styles.textInput}
+          placeholder="010-0000-0000"
+          placeholderTextColor={COLORS.TEXT_DISABLED_GREY}
+          keyboardType="number-pad"
+        />
+      </View>
+      <View style={{ marginTop: 22 }}>
+        <ButtonNext
+          onPress={() => navigation.navigate('PhoneAuth')}
+          isActivated={phone !== '' && selectedCountry !== null}
+        />
+      </View>
     </SafeAreaView>
   );
 }
