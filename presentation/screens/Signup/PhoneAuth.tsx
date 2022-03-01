1bf876f61552494b484c7d86f5817915fb462f2e
seojin kim <001106ksj@gmail.com>
2022-03-01 18:50:19 +0900

    ✨ feat: 인증번호 확인로직 추가, 타이머 추가

--- presentation/screens/Signup/PhoneAuth.tsx
+++ presentation/screens/Signup/PhoneAuth.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -45,20 +45,76 @@ const styles = StyleSheet.create({
     marginVertical: 12,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginHorizontal: 5,
   },
   resendContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 17,
+    gap: 4,
+  },
+  resendTimerText: {
+    fontFamily: 'Inter_700Bold',
+    color: COLORS.TEXT_SECONDARY,
+  },
+  resendButton: {
+    color: COLORS.TEXT_SECONDARY,
+    borderBottomColor: COLORS.TEXT_SECONDARY,
+    borderBottomWidth: 1,
   },
 });
+
 function PhoneAuthScreen({ navigation }: PhoneAuthScreenProp) {
   const inputRef1 = useRef<TextInput | null>(null);
   const inputRef2 = useRef<TextInput | null>(null);
   const inputRef3 = useRef<TextInput | null>(null);
   const inputRef4 = useRef<TextInput | null>(null);
   const inputRef5 = useRef<TextInput | null>(null);
+  const [filledTexts, setFilledTexts] = useState<string[]>([
+    '',
+    '',
+    '',
+    '',
+    '',
+  ]);
+  const [isValidate, setIsValidate] = useState(false);
+  const [minutes, setMinutes] = useState(3);
+  const [seconds, setSeconds] = useState(0);
+  const resetTime = () => {
+    setSeconds(0);
+    setMinutes(3);
+  };
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(countdown);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
+
+  useEffect(() => {
+    const answerText = 'ABCDE';
+    setIsValidate(answerText === filledTexts.join(''));
+  }, [filledTexts]);
+
+  const insertText = (text: string, index: number) => {
+    setFilledTexts((prev) => {
+      const newFilledTexts = [...prev];
+      newFilledTexts[index] = text;
+      return newFilledTexts;
+    });
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -73,6 +129,7 @@ function PhoneAuthScreen({ navigation }: PhoneAuthScreenProp) {
           autoFocus
           ref={inputRef1}
           onChangeText={(text) => {
+            insertText(text, 0);
             if (text === '') inputRef1.current?.blur();
             else inputRef2.current?.focus();
           }}
@@ -82,6 +139,7 @@ function PhoneAuthScreen({ navigation }: PhoneAuthScreenProp) {
           maxLength={1}
           ref={inputRef2}
           onChangeText={(text) => {
+            insertText(text, 1);
             if (text === '') inputRef1.current?.focus();
             else inputRef3.current?.focus();
           }}
@@ -91,6 +149,7 @@ function PhoneAuthScreen({ navigation }: PhoneAuthScreenProp) {
           maxLength={1}
           ref={inputRef3}
           onChangeText={(text) => {
+            insertText(text, 2);
             if (text === '') inputRef2.current?.focus();
             else inputRef4.current?.focus();
           }}
@@ -100,6 +159,7 @@ function PhoneAuthScreen({ navigation }: PhoneAuthScreenProp) {
           maxLength={1}
           ref={inputRef4}
           onChangeText={(text) => {
+            insertText(text, 3);
             if (text === '') inputRef3.current?.focus();
             else inputRef5.current?.focus();
           }}
@@ -109,6 +169,7 @@ function PhoneAuthScreen({ navigation }: PhoneAuthScreenProp) {
           maxLength={1}
           ref={inputRef5}
           onChangeText={(text) => {
+            insertText(text, 4);
             if (text === '') inputRef4.current?.focus();
             else inputRef5.current?.blur();
           }}
@@ -120,12 +181,16 @@ function PhoneAuthScreen({ navigation }: PhoneAuthScreenProp) {
       <View style={{ marginTop: 22 }}>
         <ButtonNext
           onPress={() => navigation.navigate('EmailInput')}
-          isActivated
+          isActivated={isValidate}
         />
       </View>
       <View style={styles.resendContainer}>
-        <Text>{}</Text>
-        <Text>재전송</Text>
+        <Text style={styles.resendTimerText}>
+          {minutes}:{seconds.toString().padStart(2, '0')}
+        </Text>
+        <Text onPress={resetTime} style={styles.resendButton}>
+          재전송
+        </Text>
       </View>
     </SafeAreaView>
   );
