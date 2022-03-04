3c1ded8e1e2b2f421590d0d49de45aecf6cf164f
seojin kim <001106ksj@gmail.com>
2022-03-05 01:17:30 +0900

    ✨ feat: 모달 스타일 씌우기

--- presentation/components/PhotoSelectorModal.tsx
+++ presentation/components/PhotoSelectorModal.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback } from 'react';
-import { Modal, Text } from 'react-native';
+import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import COLORS from '../styles/colors';
 
 type PhotoSelectorModalProp = {
   isModalVisible: boolean;
@@ -9,45 +11,80 @@ type PhotoSelectorModalProp = {
   setPickedImagePath: (path: string) => void;
 };
 
+const styles = StyleSheet.create({
+  modalContainer: {
+    backgroundColor: '#00000069',
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 11,
+  },
+  actionContainer: {
+    borderRadius: 10,
+    backgroundColor: 'white',
+    marginBottom: 8,
+  },
+  actionButton: {
+    paddingVertical: 18,
+    width: '100%',
+  },
+  actionText: {
+    fontSize: 14,
+    fontFamily: 'Pretendard',
+    textAlign: 'center',
+  },
+  bottomLiner: {
+    borderBottomColor: COLORS.GREY_030,
+    borderBottomWidth: 1,
+  },
+  cancelText: {
+    color: COLORS.THEME_NEGATIVE,
+  },
+});
+
 function PhotoSelectorModal(props: PhotoSelectorModalProp) {
   const { isModalVisible, closeModal, setPickedImagePath } = props;
   const takePhoto = useCallback(async () => {
     const permissionResult = await ImagePicker.requestCameraPermissionsAsync();
-    if (!permissionResult.granted) {
-      return;
-    }
-
+    if (!permissionResult.granted) return;
     const result = await ImagePicker.launchCameraAsync();
-    console.log(result);
-    if (!result.cancelled) {
-      setPickedImagePath(result.uri);
-    }
+    if (!result.cancelled) setPickedImagePath(result.uri);
   }, []);
 
   const selectPhoto = useCallback(async () => {
     const permissionResult =
       await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!permissionResult.granted) {
-      return;
-    }
-
+    if (!permissionResult.granted) return;
     const result = await ImagePicker.launchImageLibraryAsync();
-    console.log(result);
-    if (!result.cancelled) {
-      setPickedImagePath(result.uri);
-    }
+    if (!result.cancelled) setPickedImagePath(result.uri);
   }, []);
 
   return (
     <Modal
-      animationType="slide"
+      animationType="fade"
       transparent
       visible={isModalVisible}
       onRequestClose={closeModal}
     >
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'transparent' }}>
-        <Text onPress={selectPhoto}>사진 보관함</Text>
-        <Text onPress={takePhoto}>카메라</Text>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Pressable style={styles.modalContainer} onPress={closeModal}>
+          <View style={styles.actionContainer}>
+            <TouchableOpacity
+              onPress={takePhoto}
+              style={[styles.actionButton, styles.bottomLiner]}
+            >
+              <Text style={styles.actionText}>카메라로 촬영</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={selectPhoto} style={styles.actionButton}>
+              <Text style={styles.actionText}>앨범에서 선택</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            onPress={closeModal}
+            style={[styles.actionContainer, styles.actionButton]}
+          >
+            <Text style={[styles.actionText, styles.cancelText]}>취소하기</Text>
+          </TouchableOpacity>
+        </Pressable>
       </SafeAreaView>
     </Modal>
   );
