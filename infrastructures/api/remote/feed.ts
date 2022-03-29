3667b4fc0c6f2f57045c005be877523addeb0739
seojin kim <001106ksj@gmail.com>
2022-03-30 02:53:17 +0900

    feat: Empty View 추가

--- infrastructures/api/remote/feed.ts
+++ infrastructures/api/remote/feed.ts
@@ -1,3 +1,6 @@
+/* eslint-disable no-nested-ternary */
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ClockTime } from '../../types/feed';
 import { FeedService } from '../feed';
 import { privateAPI } from './base';
 
@@ -12,19 +15,84 @@ export function feedRemote(): FeedService {
   };
 
   const getAllFeed = async () => {
-    return privateAPI.get({ url: '/api/v1/users/auth/family-posts' });
+    return privateAPI
+      .get({ url: '/api/v1/users/auth/family-posts' })
+      .then((response) => {
+        return Object.keys(response.singleData.result).map((date) => ({
+          date,
+          feeds: response.singleData.result.date.map((feed: any) => ({
+            id: feed.id,
+            writer: feed.authorName,
+            writerID: feed.authorIdx,
+            writerThumbnail: feed.profileImgUrl,
+            title: feed.title,
+            body: feed.contents,
+            reactions: feed.reactionCount.map((reaction: any) => {
+              if (reaction.type === 'TEXT') return reaction;
+              if (reaction.type === 'IMAGE')
+                return { type: 'EMOJI', count: reaction.count };
+              if (reaction.type === 'AUDIO')
+                return { type: 'RECORD', count: reaction.count };
+              return reaction;
+            }),
+          })),
+        }));
+      });
   };
 
   const getTime = async (familyID: number) => {
-    return privateAPI.get({
-      url: `/api/v1/families/${familyID}/users/time-difference`,
-    });
+    const myID = await AsyncStorage.getItem('userID');
+    return privateAPI
+      .get({
+        url: `/api/v1/families/${familyID}/users/time-difference`,
+      })
+      .then((response) => {
+        const data: ClockTime[] = response.multipleData.map((time: any) => ({
+          id: time.id,
+          countryCode: time.countryCode,
+          timeDelta: time.timeDiff,
+        }));
+        const mine = data.find((time: any) => time.id === myID?.toString()) ?? {
+          id: +(myID ?? 0),
+          countryCode: 'KR',
+          timeDelta: 0,
+        };
+        return {
+          my: {
+            id: mine.timeDelta,
+            countryCode: mine.countryCode,
+            timeDelta: 0,
+          },
+          families: data
+            .filter((time) => time.id?.toString() !== myID)
+            .map((time) => ({
+              id: time.id,
+              countryCode: time.countryCode,
+              timeDelta: time.timeDelta - mine.timeDelta,
+            })),
+        };
+      });
   };
 
   const getReactions = async (feedID: number) => {
-    return privateAPI.get({
-      url: `/api/v1/posts/${feedID}/reactions`,
-    });
+    return privateAPI
+      .get({
+        url: `/api/v1/posts/${feedID}/reactions`,
+      })
+      .then((response) => {
+        return response.multipleData.map((reaction: any) => ({
+          writerThumbnail: reaction.profileImgUrl,
+          writerID: reaction.authorIdx,
+          reactionID: reaction.id,
+          reactionType:
+            reaction.type === 'IMAGE'
+              ? 'EMOJI'
+              : reaction.type === 'AUDIO'
+              ? 'RECORD'
+              : 'TEXT',
+          content: reaction.contents,
+        }));
+      });
   };
 
   return { createFeed, getAllFeed, getTime, getReactions };
