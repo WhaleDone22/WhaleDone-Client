d8781c818b557ccc1dc29aede2ed808f78a89319
seojin kim <001106ksj@gmail.com>
2022-03-31 23:20:12 +0900

    feat: 이미지 업로드 고치기

--- presentation/components/PhotoSelectorModal.tsx
+++ presentation/components/PhotoSelectorModal.tsx
@@ -3,7 +3,6 @@ import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import COLORS from '../styles/colors';
-import { fileURItoBLOB } from '../../infrastructures/utils/images';
 
 type PhotoSelectorModalProp = {
   isModalVisible: boolean;
@@ -53,11 +52,14 @@ function PhotoSelectorModal(props: PhotoSelectorModalProp) {
         .replaceAll('.', '')
         .replaceAll(':', '')}.png`;
       const formData = new FormData();
-      const imageBLOB = await fileURItoBLOB(imagePath);
-      const imageFile = new File([imageBLOB], fileName);
+      formData.append('multipartFile', {
+        uri: imagePath, // 에러 표시되지만 잘 작동합니다.
+        name: fileName,
+        type: 'image/png',
+      });
       const myToken = await AsyncStorage.getItem('token');
       if (!myToken) return;
-      formData.append('multipartFile', imageFile);
+
       const uploadRequest = await fetch(
         'http://ec2-3-37-42-113.ap-northeast-2.compute.amazonaws.com:8080/api/v1/content',
         {
@@ -71,7 +73,6 @@ function PhotoSelectorModal(props: PhotoSelectorModalProp) {
 
       if (typeof uploadResponse.singleData.url === 'string') {
         setPickedImagePath(uploadResponse.singleData.url);
-        console.warn(uploadResponse.singleData.url);
       }
     } catch (e) {
       console.error(e);
